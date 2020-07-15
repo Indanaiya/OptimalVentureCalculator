fc1415ce0ff62f957d631d67c6325d1ef604edbe
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-07-15 15:05:02 +0100

    Get prices for venture items from Universalis and print them.

--- src/main.py
+++ src/main.py
@@ -2,47 +2,96 @@ import json
 import requests
 import csv
 from pprint import pprint 
+import os
+from datetime import datetime, timedelta
 
 #RetainerTask.csv shows the gathering/ilvl requirement, the time and venture cost, as well as the needed retainer level and class
 #RetainerTaskNormal.csv shows what items a venture gives and how many of that item
 #Item.csv gives all the information about the items
 
-universalis_url = "https://universalis.app/api/"
-stored_item_values_address = "values.json"
-datacenter = "Chaos"
+#TODO check that the server is valid
+
+cached_prices_address = "../res/cachedPrices.json"
 job_to_ClassJobCategory = {'DoW/M': 34, 'MIN':17, 'BTN':18, 'FSH':19}#For retainer task
 #ClassJobCategory_to_job = {v:k for k,v in job_to_ClassJobCategory.items()}
 
 class RetainerOptimiser():
-    def __init__(self):
+    def __init__(self, server='Cerberus'):
         self.retainer_task_dicts = []
         self.retainer_task_normal_dicts = {}
         self.item_dicts = {}
+        self.universalis_handler = UniversalisHandler(server, update=False)
 
-        with open('res/RetainerTask.csv', 'r', encoding="UTF-8-sig") as f:
+        with open('../res/RetainerTask.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
                 self.retainer_task_dicts.append(l)
-        with open('res/RetainerTaskNormal.csv', 'r', encoding="UTF-8-sig") as f:
+        with open('../res/RetainerTaskNormal.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
                 self.retainer_task_normal_dicts[l['key']] = l
-        with open('res/Item.csv', 'r', encoding="UTF-8-sig") as f:
+        with open('../res/Item.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
                 self.item_dicts[l['key']] = l
 
-        # for dic in self.retainer_task_normal_dicts[2:]:
-        #     if dic['Item'] != '0':
-        #         print(self.item_dicts[dic['Item']])
+    def savePrices(self):
+        self.universalis_handler.save()
 
-    def getMining(self, level=1, gathering=0):
+    def getMining(self, level=1, gathering=0, quantity=2):
+        ventures = []
         for element in self.retainer_task_dicts[2:]:
             if int(element['ClassJobCategory']) == job_to_ClassJobCategory['MIN'] and int(element['RequiredGathering']) <= gathering and int(element['RetainerLevel']) <= level and element['VentureCost'] == '1':
-                item_id = self.retainer_task_normal_dicts[element['key']]['Item']
-                #print(f"Element: {element['key']}, {self.retainer_task_normal_dicts[element['key']]}")
-                print(f"Item id: {item_id}, Item Name: {self.item_dicts[item_id]['0']}")
+                venture_id = element['key']
+                item_id = self.retainer_task_normal_dicts[venture_id]['Item']
+                item_name = self.item_dicts[item_id]['0']
+                item_quantity = self.retainer_task_normal_dicts[venture_id][f"Quantity[{quantity}]"]
+                ventures.append({'item_id': item_id, 'item_name': item_name, 'venture_id': venture_id, 'item_quantity': item_quantity})
+        for v in ventures:
+            v['price'] = self.universalis_handler.getUniversalisPrice(v['item_id'])
+        for v in ventures:
+            print(v)
+
+class UniversalisHandler():
+    def __init__(self, server, update=True):
+        self.prices = self.getCachedPrices()
+        self.cache_ttl = 3 #The time to live of cached data (in days)
+        self.server = server
+        self.universalis_url = f"https://universalis.app/api/{server}/"
+        self.update = update #Prevents fetching more data from Unviversalis if False and the data needed is present in cache just outdated
+
+    def getCachedPrices(self):
+        """Load all item prices from cache"""
+        if os.path.isfile(cached_prices_address):
+            with open(cached_prices_address) as f:
+                return json.load(f)
+        else:
+            return {}
+
+    def getUniversalisPrice(self, item_id):
+        """Get the price for the given item, on the server which this class is set to get data from"""
+        if not self.server in self.prices:   
+            self.prices[self.server] = {}
+
+        item_id_in_prices = item_id in self.prices[self.server]
+        #Data fetched from universalis if there is no data for this item, or if the data we have is outdated, and self.update is True
+        if not item_id_in_prices or ((datetime.strptime(self.prices[self.server][item_id]['time'], "%Y-%m-%dT%H:%M:%S")) < (datetime.now() - timedelta(days=self.cache_ttl)) and self.update):
+            print(f"Fetching {item_id} from Universalis")
+            with requests.request("GET", self.universalis_url + item_id + "?entries=1") as response:
+                self.prices[self.server][item_id] = {}
+                self.prices[self.server][item_id]['price'] = response.json()['listings'][0]['pricePerUnit']
+                self.prices[self.server][item_id]['time'] = datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
+
+        return self.prices[self.server][item_id]['price']
+
+    def save(self):
+        """Save stored item prices to cache"""
+        if self.prices != {}:
+            with open(cached_prices_address, 'w') as f:
+                json.dump(self.prices, f)
+        
 
 if __name__ == "__main__":
     ro = RetainerOptimiser()
     ro.getMining(level=10)
+    ro.savePrices() #Necessary if you want to save Universalis data to cache

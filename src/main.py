0266f7d269cbd01b77e6821c6d6e45be7a374bb7
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-07-15 13:26:06 +0100

    Add method to get mining ventures.

--- src/main.py
+++ src/main.py
@@ -11,30 +11,38 @@ universalis_url = "https://universalis.app/api/"
 stored_item_values_address = "values.json"
 datacenter = "Chaos"
 job_to_ClassJobCategory = {'DoW/M': 34, 'MIN':17, 'BTN':18, 'FSH':19}#For retainer task
+#ClassJobCategory_to_job = {v:k for k,v in job_to_ClassJobCategory.items()}
 
 class RetainerOptimiser():
     def __init__(self):
         self.retainer_task_dicts = []
-        self.retainer_task_normal_dicts = []
+        self.retainer_task_normal_dicts = {}
         self.item_dicts = {}
 
-        with open('res/RetainerTask.csv', 'r', encoding="UTF-8") as f:
+        with open('res/RetainerTask.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
                 self.retainer_task_dicts.append(l)
-        with open('res/RetainerTaskNormal.csv', 'r', encoding="UTF-8") as f:
+        with open('res/RetainerTaskNormal.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
-                self.retainer_task_normal_dicts.append(l)
-        with open('res/Item.csv', 'r', encoding="UTF-8") as f:
+                self.retainer_task_normal_dicts[l['key']] = l
+        with open('res/Item.csv', 'r', encoding="UTF-8-sig") as f:
             reader = csv.DictReader(f)
             for l in reader:
                 self.item_dicts[l['key']] = l
 
-        for dic in self.retainer_task_normal_dicts[2:]:
-            if dic['Item'] != '0':
-                print(self.item_dicts[dic['Item']])
+        # for dic in self.retainer_task_normal_dicts[2:]:
+        #     if dic['Item'] != '0':
+        #         print(self.item_dicts[dic['Item']])
 
+    def getMining(self, level=1, gathering=0):
+        for element in self.retainer_task_dicts[2:]:
+            if int(element['ClassJobCategory']) == job_to_ClassJobCategory['MIN'] and int(element['RequiredGathering']) <= gathering and int(element['RetainerLevel']) <= level and element['VentureCost'] == '1':
+                item_id = self.retainer_task_normal_dicts[element['key']]['Item']
+                #print(f"Element: {element['key']}, {self.retainer_task_normal_dicts[element['key']]}")
+                print(f"Item id: {item_id}, Item Name: {self.item_dicts[item_id]['0']}")
 
 if __name__ == "__main__":
-    RetainerOptimiser()
+    ro = RetainerOptimiser()
+    ro.getMining(level=10)

b5306759bdb742b0aace9b610a3e22b0ef825d93
Jay <14353902+Indanaiya@users.noreply.github.com>
2020-07-15 12:55:17 +0100

    Get data from more csv. Add key for converting from code to class. Put things in a class.

--- src/main.py
+++ src/main.py
@@ -1,48 +1,40 @@
 import json
 import requests
 import csv
-import pprint
+from pprint import pprint 
+
+#RetainerTask.csv shows the gathering/ilvl requirement, the time and venture cost, as well as the needed retainer level and class
+#RetainerTaskNormal.csv shows what items a venture gives and how many of that item
+#Item.csv gives all the information about the items
 
 universalis_url = "https://universalis.app/api/"
 stored_item_values_address = "values.json"
 datacenter = "Chaos"
+job_to_ClassJobCategory = {'DoW/M': 34, 'MIN':17, 'BTN':18, 'FSH':19}#For retainer task
 
-def openCSV():
-    dictionary = []
-    with open('res/RetainerTaskNormal.csv', 'r') as f:
-        reader = csv.DictReader(f)
-        for l in reader:
-            dictionary.append(l)
-    pprint.pprint(dictionary)
+class RetainerOptimiser():
+    def __init__(self):
+        self.retainer_task_dicts = []
+        self.retainer_task_normal_dicts = []
+        self.item_dicts = {}
 
-def getItemsFromUniversalis(item_values=None):
-    with open(stored_item_values_address) as f:
-        stored_item_values = json.load(f)
-        if not item_values:
-            item_values = stored_item_values
-    item_prices = {}
-    for i in item_values:
-        with requests.request("GET", universalis_url + datacenter + "/" + item_values[i]['id'] + "?entries=1") as response:
-            response_json = response.json()
-            price = response_json['listings'][0]['pricePerUnit']
-            price_per_tomestone = float(price) / float(item_values[i]['tomestone_price'])
-            item_prices[i] = {'price': price, 'price_per_tomestone': price_per_tomestone}
-    sorted_prices = sorted(item_prices.items(), key=lambda x: x[1]['price_per_tomestone'], reverse=True)
+        with open('res/RetainerTask.csv', 'r', encoding="UTF-8") as f:
+            reader = csv.DictReader(f)
+            for l in reader:
+                self.retainer_task_dicts.append(l)
+        with open('res/RetainerTaskNormal.csv', 'r', encoding="UTF-8") as f:
+            reader = csv.DictReader(f)
+            for l in reader:
+                self.retainer_task_normal_dicts.append(l)
+        with open('res/Item.csv', 'r', encoding="UTF-8") as f:
+            reader = csv.DictReader(f)
+            for l in reader:
+                self.item_dicts[l['key']] = l
 
-    header_price_per_tomestone = "Price per Tomestone"
-    header_name = "Item(Unit Price)"
-    price_per_colwidth = max([len(str(item[1]['price_per_tomestone'])) for item in item_prices.items()] + [len(header_price_per_tomestone)])
-    header_title = f"{header_price_per_tomestone} | {header_name}"
-    
+        for dic in self.retainer_task_normal_dicts[2:]:
+            if dic['Item'] != '0':
+                print(self.item_dicts[dic['Item']])
 
-    #print(*["=" for i in range(len(header_string))], sep="")
-    data_to_print = []
-    for k, v in sorted_prices:
-        data_to_print.append(f"{str(v['price_per_tomestone']):>{price_per_colwidth}} | {str(k)}({str(v['price'])})")
-
-    print(header_title)
-    print(*["=" for i in range(max([len(string) for string in data_to_print] + [len(header_title)]))], sep="") #Prints enough = to cover the width of the widest point of the table
-    print(*data_to_print, sep="\n")
 
 if __name__ == "__main__":
-    openCSV()
+    RetainerOptimiser()
